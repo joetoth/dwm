f1df83e61cfee0f2f9a8ebc6dc5be1b467bdd16c
Joe Toth <joetoth@gmail.com>
2017-09-25 12:26:11 -0400

    p

--- config.def.h
+++ config.def.h
@@ -63,7 +63,7 @@ static const char *clipstercmd[]  = { "clipster", "-s", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_o,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_v,      spawn,          {.v = clipstercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
